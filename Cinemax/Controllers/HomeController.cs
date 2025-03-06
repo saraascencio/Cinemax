39cec89195405422d6606b65d94526e65bf8668c
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-03-05 23:55:56 -0600

    Agregar archivos de proyecto.

---
+++ Cinemax/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinemax.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult DashboardCliente()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+           
+            if (Username == "empleado@gmail.com" && Password == "12345")
+            {
+                
+                return RedirectToAction("Dashboard");
+            }
+            else if (Username == "cliente@gmail.com" && Password == "12345")
+            {
+                
+                return RedirectToAction("DashboardCliente");
+            }
+            else
+            {
+               
+                ViewBag.ErrorMessage = "Credenciales incorrectas. Inténtalo de nuevo.";
+                return View();
+            }
+        }
+
+
+
+
+        public ActionResult Registro()
+        {
+            return View(); 
+        }
+    }
+}
