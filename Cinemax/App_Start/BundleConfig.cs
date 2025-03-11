2bd86063e24cbf8535a1a234b26f9c794a83bc58
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-03-10 22:40:35 -0600

    Resolviendo problemas (se probó clonarlo para verificar buen funcionamiento)

--- Cinemax/App_Start/BundleConfig.cs
+++ Cinemax/App_Start/BundleConfig.cs
@@ -11,19 +11,13 @@ namespace Cinemax
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
                         "~/Content/assets/libs/jquery/dist/jquery.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información sobre los formularios.  De esta manera estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
+           
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+
                       "~/Content/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
