26ddfe2b05575b8b1ee6ceeb6a2039b9ee9a9cd0
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-04-17 17:45:13 -0600

    Creación de modelos + configuración de login resuelto

--- Cinemax/Controllers/HomeController.cs
+++ Cinemax/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cinemax.Models;
+using Cinemax.Servicios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,52 +10,128 @@ namespace Cinemax.Controllers
 {
     public class HomeController : Controller
     {
-       
-        public ActionResult Login()
-        {
-            return View();
-        }
+        private CinemaxEntities _dbContext;
 
-        public ActionResult Dashboard()
+        public HomeController()
         {
-            return View();
-        }
-
-        public ActionResult DashboardCliente()
-        {
-            return View();
+            _dbContext = new CinemaxEntities();
         }
 
 
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login");
+        }
+
+
+        public ActionResult Login(bool? error)
+        {
+
+            if (error == false)
+            {
+                ViewData["ErrorMessage"] = null;
+            }
+            else if (Session["usuarioId"] != null)
+            {
+                return RedirectToDashboard();
+            }
+
+            return View();
+        }
 
         [HttpPost]
-        public ActionResult Login(string Username, string Password)
+        public ActionResult Login(string txtUsuario, string txtClave)
         {
-           
-            if (Username == "empleado@gmail.com" && Password == "12345")
+            var usuario = _dbContext.Usuario
+                  .Include("Rol")
+                  .FirstOrDefault(u => u.USU_Email == txtUsuario
+                                   && u.USU_Password == txtClave
+                                   && (u.Rol.ROL_Nombre == "Cliente" || u.Rol.ROL_Nombre == "Empleado"));
+
+            if (usuario != null)
             {
-                
-                return RedirectToAction("Dashboard");
+
+                Session["usuarioId"] = usuario.ID_Usuario;
+                Session["TipoUsuario"] = usuario.Rol.ROL_Nombre;
+                Session["Nombre"] = usuario.USU_Nombre;
+
+                return RedirectToDashboard();
             }
-            else if (Username == "cliente@gmail.com" && Password == "12345")
-            {
-                
-                return RedirectToAction("DashboardCliente");
-            }
-            else
-            {
-               
-                ViewBag.ErrorMessage = "Credenciales incorrectas. Inténtalo de nuevo.";
-                return View();
-            }
+
+            ViewData["ErrorMessage"] = "Error, usuario inválido";
+            return View();
         }
 
 
+        [Autenticacion]
+        public ActionResult Dashboard()
+        {
+            if (Session["TipoUsuario"] as string != "Empleado")
+            {
+                return RedirectToAction("Login");
+            }
+
+            ViewBag.Nombre = Session["Nombre"];
+            ViewBag.TipoUsuario = Session["TipoUsuario"];
+            return View();
+        }
+
+
+        [Autenticacion]
+        public ActionResult DashboardCliente()
+        {
+            if (Session["TipoUsuario"] as string != "Cliente")
+            {
+                return RedirectToAction("Login");
+            }
+
+            ViewBag.Nombre = Session["Nombre"];
+            ViewBag.TipoUsuario = Session["TipoUsuario"];
+            return View();
+        }
 
 
         public ActionResult Registro()
         {
-            return View(); 
+            return View();
+        }
+
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            Session.Abandon();
+
+
+            if (Request.Cookies["CinemaxSession"] != null)
+            {
+                var cookie = new HttpCookie("CinemaxSession")
+                {
+                    Expires = DateTime.Now.AddDays(-1)
+                };
+                Response.Cookies.Add(cookie);
+            }
+
+            return RedirectToAction("Login", new { error = false });
+        }
+
+        // Método redirección automática
+        private ActionResult RedirectToDashboard()
+        {
+            var tipoUsuario = Session["TipoUsuario"] as string;
+            return tipoUsuario == "Cliente"
+                ? RedirectToAction("DashboardCliente")
+                : RedirectToAction("Dashboard");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && _dbContext != null)
+            {
+                _dbContext.Dispose();
+                _dbContext = null;
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ Cinemax/Servicios/AutenticationAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinemax.Servicios
+{
+    public class AutenticacionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            var usuarioId = filterContext.HttpContext.Session["usuarioId"] as int?;
+
+            if (usuarioId == null)
+            {
+
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary
+                    {
+                    { "controller", "Home" },
+                    { "action", "Login" }
+                    });
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
