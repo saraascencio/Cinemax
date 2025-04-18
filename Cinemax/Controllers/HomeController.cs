7dd48b0944b744973be8e3bb6ff0556b415a8a30
saraascencio <sara.ascencio1@catolica.edu.sv>
2025-04-18 13:42:27 -0600

    Vista empleado (Visualización de reservas)

--- Cinemax/Controllers/HomeController.cs
+++ Cinemax/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Cinemax.Models;
 using Cinemax.Servicios;
+using Cinemax.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -133,5 +134,54 @@ namespace Cinemax.Controllers
             }
             base.Dispose(disposing);
         }
+
+        public ActionResult HistorialReservas()
+        {
+            
+            var datosReservas = (from r in _dbContext.Reserva
+                             join f in _dbContext.Funcion on r.ID_Funcion equals f.ID_Funcion
+                             join p in _dbContext.Pelicula on f.ID_Pelicula equals p.ID_Pelicula
+                             join s in _dbContext.Sala on f.ID_Sala equals s.ID_Sala
+                             join er in _dbContext.EstadoReserva on r.ID_REstado equals er.ID_Estadoreserva
+                             join b in _dbContext.Boleto on r.ID_Reserva equals b.ID_Reserva into boletos
+                             from b in boletos.DefaultIfEmpty()
+                             join a in _dbContext.Asiento on (b == null ? null : (int?)b.ID_Asiento) equals a.ID_Asiento into asientos
+                             from a in asientos.DefaultIfEmpty()
+                             // Utilizamos esta operación de agrupamiento de objetos anónimos
+                             // Con el fin de faciltar las operaciones por grupo bajo un id reserva
+                             group new { r, f, p, s, er, a } by r.ID_Reserva into g
+                             select new
+                             {
+                                 ID_Reserva = g.Key,
+                                 FuncionFecha = g.Select(x => x.f.FUN_Fechahora).FirstOrDefault(),
+                                 PeliculaNombre = g.Select(x => x.p.PEL_Titulo).FirstOrDefault(),
+                                 Precio = g.Select(x => x.f.FUN_Precio).FirstOrDefault(),
+                                 SalaNombre = g.Select(x => x.s.SAL_Nombre).FirstOrDefault(),
+                                 AsientosData = g.Where(x => x.a != null)
+                                               .Select(x => new {
+                                                   Fila = x.a.ASI_Fila,
+                                                   Numero = x.a.ASI_Numero
+                                               }),
+                                 QR = g.Select(x => x.r.RES_QR).FirstOrDefault(),
+                                 Estado = g.Select(x => x.er.ESR_Estado).FirstOrDefault()
+                             }).AsEnumerable(); 
+
+            // Se optó por ViewModel para garantizar mejor funcionamiento
+            // por la unión de varias tablas que se da
+            var listadoFinal = datosReservas.Select(x => new ReservaViewModel
+            {
+                ID_Reserva = x.ID_Reserva,
+                FuncionFecha = x.FuncionFecha,
+                PeliculaNombre = x.PeliculaNombre,
+                Precio = x.Precio,
+                SalaNombre = x.SalaNombre,
+                Asientos = string.Join(", ", x.AsientosData.Select(a => $"{a.Fila}{a.Numero}").Distinct()),
+                QR = x.QR,
+                Estado = x.Estado
+            }).ToList();
+
+            return View(listadoFinal);
+        }
+
     }
 }
---
+++ Cinemax/ViewModels/ReservaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinemax.ViewModels
+{
+    public class ReservaViewModel
+    {
+        public int ID_Reserva { get; set; }
+        public DateTime FuncionFecha { get; set; }
+        public string PeliculaNombre { get; set; }
+        public decimal Precio { get; set; }
+        public string SalaNombre { get; set; }
+        public string Asientos { get; set; }
+        public string QR { get; set; }
+        public string Estado { get; set; }
+    }
+}
