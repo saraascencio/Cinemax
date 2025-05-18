d8888ff6a3076d978fd5607f38b1ce2495f5b61f
ErnestoT04 <eernestoj21@gmail.com>
2025-05-18 13:20:09 -0600

    Añade funcionalidad de cartelera de peliculas: 1. Configuracion de layout para cliente

---
+++ Cinemax/Controllers/CarteleraController.cs
@@ -0,0 +1,37 @@
+﻿using Cinemax.Models;
+using Cinemax.Servicios;
+using Cinemax.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinemax.Controllers
+{
+    public class CarteleraController : Controller
+    {
+        private CinemaxEntities db = new CinemaxEntities();
+
+        // GET: Cartelera
+        public ActionResult Index(DateTime? fecha, string genero)
+        {
+            DateTime fechaBusqueda = fecha?.Date ?? DateTime.Today;
+
+            var funciones = db.Funcion
+         .Include(f => f.Pelicula.Genero)
+         .Where(f => DbFunctions.TruncateTime(f.FUN_Fechahora) == fechaBusqueda)
+         .ToList()
+         .Where(f => string.IsNullOrEmpty(genero) || f.Pelicula.Genero.GEN_Nombre == genero)
+         .GroupBy(f => f.Pelicula)
+         .Select(g => g.Key)
+         .ToList();
+
+            ViewBag.Generos = db.Genero.Select(g => g.GEN_Nombre).ToList();
+            ViewBag.FechaSeleccionada = fechaBusqueda;
+            ViewBag.GeneroSeleccionado = genero;
+            return View(funciones);
+        }
+    }
+}
