518020dadeaad45b4c7f76ce5bbb450f9499fa91
ErnestoT04 <eernestoj21@gmail.com>
2025-05-18 18:55:41 -0600

    Arreglando problemas de lectura del controllador.

--- Cinemax/Controllers/CarteleraController.cs
+++ Cinemax/Controllers/CarteleraController.cs
@@ -15,23 +15,41 @@ namespace Cinemax.Controllers
         private CinemaxEntities db = new CinemaxEntities();
 
         // GET: Cartelera
-        public ActionResult Index(DateTime? fecha, string genero)
+        public ActionResult Index(DateTime? fecha, string genero, int pagina = 1)
         {
             DateTime fechaBusqueda = fecha?.Date ?? DateTime.Today;
+            int peliculasPorPagina = 6;
 
             var funciones = db.Funcion
-         .Include(f => f.Pelicula.Genero)
-         .Where(f => DbFunctions.TruncateTime(f.FUN_Fechahora) == fechaBusqueda)
-         .ToList()
-         .Where(f => string.IsNullOrEmpty(genero) || f.Pelicula.Genero.GEN_Nombre == genero)
-         .GroupBy(f => f.Pelicula)
-         .Select(g => g.Key)
-         .ToList();
+                .Include(f => f.Pelicula.Genero)
+                .Where(f => DbFunctions.TruncateTime(f.FUN_Fechahora) == fechaBusqueda)
+                .ToList()
+                .Where(f => string.IsNullOrEmpty(genero) || f.Pelicula.Genero.GEN_Nombre == genero)
+                .GroupBy(f => f.Pelicula)
+                .Select(g => g.Key)
+                .ToList();
+
+            int totalPeliculas = funciones.Count();
+            int totalPaginas = (int)Math.Ceiling((double)totalPeliculas / peliculasPorPagina);
+
+            var peliculasPaginadas = funciones
+                .Skip((pagina - 1) * peliculasPorPagina)
+                .Take(peliculasPorPagina)
+                .ToList();
+
+            var viewModel = new CarteleraViewModel
+            {
+                Peliculas = peliculasPaginadas,
+                PaginaActual = pagina,
+                TotalPaginas = totalPaginas
+            };
 
             ViewBag.Generos = db.Genero.Select(g => g.GEN_Nombre).ToList();
             ViewBag.FechaSeleccionada = fechaBusqueda;
             ViewBag.GeneroSeleccionado = genero;
-            return View(funciones);
+
+            return View(viewModel);
         }
+
     }
 }
