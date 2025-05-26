6415060f5c7990e693c13098b0bd5687969f8f05
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-05-26 13:11:17 -0600

    vistas de Pago y Estado del pago
    falta redirigir e immplementar logica al controlador

---
+++ Cinemax/Controllers/PagoController.cs
@@ -0,0 +1,25 @@
+﻿using Cinemax.Models;
+using Cinemax.Servicios;
+using Cinemax.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Cinemax.Controllers
+{
+    public class PagoController : Controller
+    {
+        private CinemaxEntities db = new CinemaxEntities();
+
+        public ActionResult Pago() { 
+            return View();
+        }
+
+        public ActionResult PagoAprovado()
+        {
+            return View();
+        }
+    }
+}
