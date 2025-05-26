093f12ac42f1f57ca5b4eb9029c4c3441c8c083e
LuisS-byte <luis.sarmiento@catolica.edu.sv>
2025-05-26 13:30:47 -0600

    pago y estado pago realizado
    logica en el controller y las vistas hechas

--- Cinemax/Controllers/PagoController.cs
+++ Cinemax/Controllers/PagoController.cs
@@ -1,20 +1,58 @@
 ﻿using Cinemax.Models;
-using Cinemax.Servicios;
-using Cinemax.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+
 namespace Cinemax.Controllers
 {
     public class PagoController : Controller
     {
         private CinemaxEntities db = new CinemaxEntities();
 
-        public ActionResult Pago() { 
+
+        /*
+        // hacerlo desde el controlador que se va a llamar
+return RedirectToAction("Pago", "Pago", new { id_Reserva = reservaId });
+        */
+        public ActionResult Pago(int id_Reserva=41)
+        {
+            TempData["id_Reserva"] = id_Reserva;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProcesarPago(FormCollection form)
+        {
+            try
+            {
+                int id_Reserva = (int)TempData["id_Reserva"];
+                string email = form["email"];
+                int metodoPago = Convert.ToInt32(form["metodoPago"]);
+                if (!email.Contains(".com"))
+                {
+                    TempData["ErrorPago"] = "Su pago ha fallado. Tu pago a través de PayPal no se pudo completar. Esto puede deberse a fondos insuficientes, un problema con la tarjeta asociada, restricciones en tu cuenta o un error temporal en la plataforma.";
+                    return RedirectToAction("Pago", new { id_Reserva = id_Reserva });
+                }
+                int idMetodo = metodoPago == 2 ? 2 : 3;
+
+                var nuevoPago = new Pago
+                {
+                    ID_Reserva = id_Reserva,
+                    ID_Metodo = idMetodo,
+                    ID_Estadopago = 1,
+                    PAG_Fecha = DateTime.Now
+                };
+
+                db.Pago.Add(nuevoPago);
+                db.SaveChanges();
+
+                return RedirectToAction("PagoAprovado");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorPago"] = "Ocurrió un error al procesar el pago: " + ex.Message;
+                return RedirectToAction("Pago", new { id_Reserva = TempData["id_Reserva"] });
+            }
         }
 
         public ActionResult PagoAprovado()
