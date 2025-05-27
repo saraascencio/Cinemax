bfd6416af7e22b1129701b40a57019a35f26dd0c
ErnestoT04 <eernestoj21@gmail.com>
2025-05-27 11:28:21 -0600

    Funcionalidad para ver el historial de las reservas de un cliente

---
+++ Cinemax/ViewModels/HistorialReservaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Cinemax.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Cinemax.ViewModels
+{
+    public class HistorialReservaViewModel
+    {
+      
+            public int Id { get; set; }
+            public string Fecha { get; set; }
+            public string Pelicula { get; set; }
+            public decimal Precio { get; set; }
+            public string Sala { get; set; }
+            public string Asientos { get; set; }
+            public string CodigoQR { get; set; }
+            public string Estado { get; set; }
+        
+    }
+}
